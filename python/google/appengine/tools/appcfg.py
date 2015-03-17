bcc0d7fc5bd3534730ad6784ca7ae9000c681ad5
Craig Silverstein <csilvers@khanacademy.org>
2015-03-17 15:29:17 -0700

    Change logging for the individual scanned files from info() to debug().
    Summary:
    We upload 8000 files or so, and and even larger number we don't
    upload.  So that's 25000 loglines per module per deploy.  We don't
    need that.
    
    Test Plan: Fingers crossed.
    
    Reviewers: chris, dylan
    
    Subscribers: dylan
    
    Differential Revision: https://phabricator.khanacademy.org/D16813

--- python/google/appengine/tools/appcfg.py
+++ python/google/appengine/tools/appcfg.py
@@ -2530,7 +2530,7 @@ class AppVersionUpload(object):
                         '(max %d bytes, file is %d bytes).%s',
                         path, max_size, file_length, extra_msg)
         else:
-          logging.info('Processing file \'%s\'', path)
+          logging.debug('Processing file \'%s\'', path)
           self.AddFile(path, file_handle)
       finally:
         file_handle.close()
@@ -2671,12 +2671,12 @@ def FileIterator(base, skip_files, runtime, separator=os.path.sep):
 
       if os.path.isfile(fullname):
         if skip_files.match(name):
-          logging.info('Ignoring file \'%s\': File matches ignore regex.', name)
+          logging.debug('Ignoring file \'%s\': File matches ignore regex.', name)
         else:
           yield name
       elif os.path.isdir(fullname):
         if skip_files.match(name):
-          logging.info(
+          logging.debug(
               'Ignoring directory \'%s\': Directory matches ignore regex.',
               name)
         else:
