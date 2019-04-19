9bda0fc095750968c9447b3b4deb493d3b70dd93
Ben Kraft <benkraft@khanacademy.org>
2019-04-18 18:13:50 -0700

    Add a bit of framework for the datastore API, and implement allocateIds
    Summary:
    In this commit, I add a bit more framework beyond the basic HTTP -- this
    handles parsing and returning JSON, such that for each call we need only
    implement the actual logic.  I also added some scaffolding for tests
    (piggybacking on what devappserver already has).  As an example, I
    implemented the allocateIds call.  (I'm not sure we need it, but it is
    easily the simplest call in the REST API.)
    
    The translation itself was not too hard.  The main choice I made was to
    give up on trying to match the prod API's error responses: doing so
    would have at least doubled the length of this diff, and the Google
    emulator doesn't even match.  But I tried to at least do most of the
    same assertions.  I tried to extract out parts of the code I expect to
    be useful again for other calls.
    
    The tests are also not too hard; devappserver's utils are a bit weird to
    use but I wrapped them up and the test logic is quite nice, I think.
    
    Test Plan:
    For automated tests:
    ```
    python/run_tests.py google.appengine.tools.devappserver2.datastore_translator.handlers_test
    ```
    
    To test over HTTP: I ran the dev server (translator on port 8001) with
    ```
    python/dev_appserver.py --enable_datastore_translator=true \
        --dev_appserver_log_level=debug \
        python/demos/python/guestbook/app.yaml
    ```
    and the Google emulator (on port 8081) with
    ```
    gcloud beta emulators datastore start --project khan-academy
    ```
    then ran some requests like
    ```
    http -v POST :8081/v1/projects/khan-academy:allocateIds \
        keys:='[{"path": [{"kind": "Foo"}]}]'
    http -v POST :8081/v1/projects/khan-academy:allocateIds \
        keys:='[{"path": [{"kind": "Foo", "id": "1234"}, {"kind": "Bar"}]}]'
    ```
    against each, comparing the responses by eye (since often they are not
    identical, just more or less equivalent).  In some cases, I compared to
    prod using the API tester at
        https://cloud.google.com/datastore/docs/reference/data/rest/v1/projects/allocateIds
    
    Reviewers: csilvers
    
    Subscribers: colin, aasmund
    
    Differential Revision: https://phabricator.khanacademy.org/D53638

---
+++ python/google/appengine/tools/devappserver2/datastore_translator/grpc.py
@@ -0,0 +1,65 @@
+"""Utilities related to gRPC.
+
+We implement the REST API, not gRPC, but the error codes are still structured
+as for gRPC.  This implements some simple wrappers.
+"""
+
+_GRPC_CODE_TO_HTTP_STATUS = {
+  # Copied from, and further documented at
+  # https://github.com/grpc/grpc/blob/master/doc/statuscodes.md
+  'OK': 200,
+  'CANCELLED': 499,
+  'UNKNOWN': 500,
+  'INVALID_ARGUMENT': 400,
+  'DEADLINE_EXCEEDED': 504,
+  'NOT_FOUND': 404,
+  'ALREADY_EXISTS': 409,
+  'PERMISSION_DENIED': 403,
+  'UNAUTHENTICATED': 401,
+  'RESOURCE_EXHAUSTED': 429,
+  'FAILED_PRECONDITION': 400,
+  'ABORTED': 409,
+  'OUT_OF_RANGE': 400,
+  'UNIMPLEMENTED': 501,
+  'INTERNAL': 500,
+  'UNAVAILABLE': 503,
+  'DATA_LOSS': 500,
+}
+
+
+class Error(Exception):
+  """Return an error to the user.
+
+  This is formatted roughly like the ones from the real Google API.  Note
+  that to simplify the translator, in general we make no attempt to match its
+  actual error text.  (In fact, even the official Google emulator doesn't
+  match the prod API precisely!)  We just try to give something vaguely
+  useful where we can.
+
+  Init Arguments:
+    grpc_code (str): a GRPC status code (one of the keys from
+        _GRPC_CODE_TO_HTTP_STATUS, above)
+    message (str): a human readable error message
+
+  TODO(benkraft): Include the "details" field as the real API does (e.g. for
+  invalid keys in the JSON).
+  """
+  def __init__(self, grpc_code, message):
+    if grpc_code in _GRPC_CODE_TO_HTTP_STATUS:
+      self.grpc_code = grpc_code
+    else:
+      self.grpc_code = 'UNKNOWN'
+    self.message = message
+
+  @property
+  def http_code(self):
+    return _GRPC_CODE_TO_HTTP_STATUS[self.grpc_code]
+
+  def as_serializable(self):
+    return {
+      'error': {
+        'code': self.http_code,
+        'status': self.grpc_code,
+        'message': self.message,
+      }
+    }
--- python/google/appengine/tools/devappserver2/datastore_translator/handlers.py
+++ python/google/appengine/tools/devappserver2/datastore_translator/handlers.py
@@ -1,9 +1,118 @@
+"""Handlers for the REST APIs.
+
+This includes the toplevel translation logic for each RPC, although common
+parts are generally extracted out to separate files.
+"""
 from __future__ import absolute_import
+
+import json
+import logging
+import os
 
 import webapp2
 
-class PingHandler(webapp2.RequestHandler):
+from google.appengine.api import datastore
+from google.appengine.tools.devappserver2.datastore_translator import grpc
+from google.appengine.tools.devappserver2.datastore_translator import (
+  translate_key)
+
+
+class Ping(webapp2.RequestHandler):
+  """Handler that returns a simple 200, as a health check."""
   def get(self):
     self.response.status_int = 200
     self.response.content_type = 'text/plain'
     self.response.write('Ok')
+
+
+class _DatastoreApiHandlerBase(webapp2.RequestHandler):
+  """Base handler for Datastore REST API requests.
+
+  Handlers should subclass this, and implement json_post, which should accept
+  and return JSON (or may call self.error() and return nothing to return an
+  error).
+  """
+  def json_post(self, project_id, json_input):
+    """Extension point for subclasses to implement their logic.
+
+    This will be called (with project_id as the project ID and json_input as
+    the POST data parsed from JSON.  It should return a JSON-serializable
+    response, or call self.error() and return nothing.
+    """
+    raise NotImplementedError("Subclasses must implement!")
+
+  def post(self, project_id):
+    try:
+      # Check that the project IDs match (up to dev~ which devappserver adds).
+      devappserver_project_id = os.environ.get('APPLICATION_ID')
+      if 'dev~%s' % project_id != devappserver_project_id:
+        raise grpc.Error("INVALID_ARGUMENT",
+                         "requested project ID %s does not match "
+                         "devappserver's project ID %s" % (
+                             project_id, devappserver_project_id))
+
+      # TODO(benkraft): Assert that project_id matches the project ID this
+      # server was started with (which is the actual one that the App Engine
+      # API will use when talking to the sqlite db).
+      self.response.content_type = 'application/json'
+      if self.request.headers.get('content-type') != 'application/json':
+        raise grpc.Error("INVALID_ARGUMENT", "Missing content-type header.")
+
+      try:
+        json_input = json.loads(self.request.body)
+      except Exception as e:
+        raise grpc.Error("INVALID_ARGUMENT", "Invalid JSON: %s" % e)
+
+      try:
+        self.response.status_int = 200
+        json_output = self.json_post(project_id, json_input)
+        json.dump(json_output, self.response)
+      except grpc.Error:
+        raise
+      except KeyError as e:   # a common case, we just guess it's a bad request
+        raise grpc.Error("INVALID_ARGUMENT",
+                         "Invalid request: missing key %s." % e)
+      except Exception as e:
+        raise grpc.Error("UNKNOWN", "Internal server error: %s" % e)
+
+    except grpc.Error as e:
+      self.response.status_int = e.http_code
+      if e.http_code >= 500:
+        logging.error(e)
+      else:
+        logging.debug(e)
+      json.dump(e.as_serializable(), self.response)
+      return
+
+
+class AllocateIds(_DatastoreApiHandlerBase):
+  """Translate the REST allocateIds call (to App Engine's AllocateIds)."""
+  def json_post(self, project_id, json_input):
+    keys = json_input.get('keys')
+    if not keys:
+      # Strangely, the API returns an empty OK for any missing or empty keys.
+      return {}
+
+    returned_keys = []
+    for key in keys:
+      gae_key = translate_key.rest_to_gae(key, project_id, incomplete=True)
+
+      # The REST API is structured such that you pass a bunch of incomplete
+      # keys; in App Engine instead you pass a single incomplete key and a
+      # count of how big a range you'd like.  (Note the returned range is
+      # inclusive.)
+      # TODO(benkraft): Do these requests async (if there are multiple).
+      # (This may require making the tests depend on ordering less.)
+      start, end = datastore.AllocateIds(gae_key, size=1)
+      if end < start:
+        # Check that we got back an allocation!  This should "never" happen.
+        raise grpc.Error("INTERNAL", "Unable to allocate IDs.")
+
+      # Sadly, there's no documented way to modify a datastore key in place.
+      # We do it anyway -- copying would be a bunch more work.  (This is just
+      # setting the id of the last element of the key-path.)
+      gae_key._Key__reference.path().element(-1).set_id(start)
+
+      returned_keys.append(translate_key.gae_to_rest(gae_key))
+
+    return {'keys': returned_keys}
